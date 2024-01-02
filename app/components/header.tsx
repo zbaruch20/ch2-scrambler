083f888eaea568bb0fe4e6200fffa538d1dee39f
Zach Baruch <zbaruch20@gmail.com>
2024-01-01 22:39:29 -0500

    feat: navbar updates, cubicle link

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,14 +1,27 @@
 import { Container, Nav, NavLink, Navbar, NavbarBrand, NavbarCollapse } from "react-bootstrap";
+import Image from "next/image";
+import cubicle from "../../public/cubicle-wireframe.svg"
 
 export default function Header() {
   return (
     <Navbar expand="md" bg="primary">
       <Container>
-        <NavbarBrand className="text-light">Corner Helicopter 2x2 Scrambler</NavbarBrand>
+        <NavbarBrand
+          className="text-light h1"
+          style={{ fontSize: "32px" }}
+        >Corner Helicopter 2x2 Scrambler</NavbarBrand>
 
         <NavbarCollapse className="justify-content-end">
           <Nav>
-            <NavLink className="text-light">Foo</NavLink>
+            <NavLink href="https://www.thecubicle.com/products/yuxin-corner-helicopter-2x2?_pos=1&_psq=corner%20hel&_ss=e&_v=1.0" target="_blank">
+              <Image
+                src={cubicle}
+                alt="Purchase link for TheCubicle"
+                title="Purchase link for TheCubicle"
+                width={50}
+                height={50}
+              />
+            </NavLink>
             <NavLink className="text-light">Bar</NavLink>
           </Nav>
         </NavbarCollapse>
